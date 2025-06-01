b311ed54c58b648c094da9f6e20aad784662dfcc
Pierce Brooks <piercebrooks@rocketmail.com>
2025-06-01 19:36:38 -0400

    more work

--- linux.sh
+++ linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install qtbase5-dev qttools5-dev libyaml-cpp-dev libunshield-dev libboost-all-dev
+sudo apt-get install qtbase5-dev qttools5-dev libyaml-cpp-dev libunshield-dev libboost-all-dev librsvg2-dev
 python3 -m pip install -r $PWD/Buildster/requirements.txt
 python3 $PWD/Buildster/buildster/buildster.py $PWD/Project.xml
 

d50f0d49a14923617d0f631321458f8e04703b2c
Pierce Brooks <piercebrooks@rocketmail.com>
2025-06-03 04:04:20 -0400

    undo debuggery

--- darwin.sh
+++ darwin.sh
@@ -2,5 +2,5 @@
 
 brew install luajit qt@5 boost ffmpeg freetype
 python3 -m pip install -r $PWD/Buildster/requirements.txt
-python3 $PWD/Buildster/buildster/buildster.py $PWD/Project.xml BUILDSTER_QT=`brew --prefix qt@5` BUILDSTER_FREETYPE=`brew --prefix freetype` BUILDSTER_BUILD=TES3MP
+python3 $PWD/Buildster/buildster/buildster.py $PWD/Project.xml BUILDSTER_QT=`brew --prefix qt@5` BUILDSTER_FREETYPE=`brew --prefix freetype`
 
